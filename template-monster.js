65f7fa61aa06ed40a0474afbad556870bb7cb177
Ryan Detzel <ryan.detzel@gmail.com>
2012-12-06 14:38:41 +0000

    cleaned up some spacing and removed consols

--- template-monster.js
+++ template-monster.js
@@ -10,57 +10,54 @@
   };
 
   templateLoader.loadTemplates = function(_templates){
-		for(var template in _templates){
-			templateLoader.loadTemplate(_templates[template]);
-		}
-	};
-	function localStorageAvailable(){
-       try {
-          return 'localStorage' in window && window['localStorage'] !== null;
-        } catch (e) {
-          return false;
-        }
-    };
-	function cache(data, callback){
-		templateLoader.templates[data['name']] = data;
-		callback(data);
-	};
-	templateLoader.loadTemplate = function(templateInfo){
-		var name = templateInfo['name'];
-		var filename = templateInfo['filename'];
-		var callback = templateInfo['callback'] || function(data){};
-		
-		/* Check that it's not already loaded! */
-		//console.log(templateLoader.templates);
-		if (templateLoader.templates[name] && templateLoader.templates[name]['filename'] == filename){
-			console.log("Already in cache");
-			callback(templateLoader.templates[name]);
-			return;
-		}
-		/* Checks local storage, if it's there does a version check otherwise pull from server */
-		console.log("Loading: " + name + " " + filename);
-		if (localStorageAvailable()){
-			var data = localStorage.getItem(name);
-			data = data && JSON.parse(data);
-			if (data && data['filename'] && data['filename'] === filename){
-				console.log("Returning local storage version");
-				cache(data, callback);
-				return;
-			}
-		}
-		//Pull from server
-		jQuery.get(filename, function(data) {
-			console.log("Pulling from server");
-			var obj = {name:name, filename:filename, data:data};
-			cache(obj, callback);
-			saveTemplate(obj);
-		});
-	};
-	function saveTemplate(data){
-		//templateLoader.templates[name] = obj;
-		if (localStorageAvailable()){
-			console.log("Saving local storage: " + data['name']);
-			localStorage.setItem(data['name'], JSON.stringify(data));
-		}
+    for(var template in _templates){
+      templateLoader.loadTemplate(_templates[template]);
+    }
+  };
+
+  function localStorageAvailable(){
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null;
+    }catch (e){
+      return false;
+    }
+  };
+
+  function cache(data, callback){
+    emplateLoader.templates[data['name']] = data;
+    callback(data);
+  };
+
+  templateLoader.loadTemplate = function(templateInfo){
+    var name = templateInfo['name'];
+    var filename = templateInfo['filename'];
+    var callback = templateInfo['callback'] || function(data){};
+    
+    /* Check that it's not already loaded! */
+    if (templateLoader.templates[name] && templateLoader.templates[name]['filename'] == filename){
+      callback(templateLoader.templates[name]);
+      return;
+    }
+    /* Checks local storage, if it's there does a version check otherwise pull from server */
+    if (localStorageAvailable()){
+      var data = localStorage.getItem(name);
+      data = data && JSON.parse(data);
+      if (data && data['filename'] && data['filename'] === filename){
+        cache(data, callback);
+        return;
+      }
+    }
+
+    jQuery.get(filename, function(data) {
+      var obj = {name:name, filename:filename, data:data};
+      cache(obj, callback);
+      saveTemplate(obj);
+    });
+  };
+
+  function saveTemplate(data){
+    if (localStorageAvailable()){
+      localStorage.setItem(data['name'], JSON.stringify(data));
+    }
   };
 })(window.templateLoader = window.templateLoader || {}, jQuery);
